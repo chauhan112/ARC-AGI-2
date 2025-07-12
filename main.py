a0ee99c33da346224aafca4183ae57f1207d6106
raja <rajababuchauhan500@gmail.com>
2025-07-12 21:12:26 +0200

    refactored past code

--- main.py
+++ main.py
@@ -3,32 +3,18 @@ import sys
 sys.path.insert(0, 'C:\\Users\\rajab\\Desktop\\cloud\\cloud\\Global\\code\\libs\\RLibs')
 from timeline.t2024.arc.Viewer import ARCViewer
 from src.reader import Reader
+from src.tools import Labels, ArrayTools, Field
+from src.tools import ArcQuestion, Labels
 
-# %%
 train_path = r"C:\Users\rajab\Desktop\stuffs\TimeLine\2025\07_July\ARC-AGI-2\data\arc-agi_training_challenges.json"
 reader = Reader(train_path)
 viewer = ARCViewer()
 viewer.process.model.process.fileLoc = train_path
 viewer.process.model.handlers.load()
 
+# %%
+ques = ArcQuestion(reader.data["00d62c1b"])
+ArrayTools.copy2Clipboard(ques.get(4, Labels.output))
 
 # %%
-# Question 1
-from src.tools import ArcQuestion, Labels
-from src.solver import sol_00576224
-ques = ArcQuestion(reader.data["00576224"])
-ques.verify(sol_00576224)
-
-# %%
-# Question 2
-from src.tools import ArcQuestion
-from src.solver import sol_007bbfb7
-ques2 = ArcQuestion(reader.data["007bbfb7"])
-ques2.verify(sol_007bbfb7)
-
-# %%
-# Question 3
-from src.tools import ArcQuestion
-from src.solver import sol_009d5c81
-ques3 = ArcQuestion(reader.data["009d5c81"])
-ques3.verify(sol_009d5c81)
+ArrayTools.copy2Clipboard(ques.get(1, Labels.input))
