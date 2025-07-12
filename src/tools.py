bcc5dbd3d3018759df9699506bc8d08b7ffa9606
raja <rajababuchauhan500@gmail.com>
2025-07-12 21:13:44 +0200

    copy to clip, subgrid, boundingRect area, flatten tool func

--- src/tools.py
+++ src/tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Set, Tuple, List
 class Labels:
     train ="train"
     test = "test"
@@ -9,7 +10,7 @@ class ArcQuestion:
     def __init__(self, question):
         self.question = question
     def shape(self,idx=0, inputOrOutput= Labels.input):
-        return len(self.question[Labels.train][idx][inputOrOutput]), len(self.question["train"][idx][inputOrOutput][0])
+        return ArrayTools.shape(self.question[Labels.train][idx][inputOrOutput])
     def get(self,idx=0, inputOrOutput= Labels.input):
         return self.question[Labels.train][idx][inputOrOutput]
     def verify(self, solver):
@@ -26,7 +27,25 @@ class ArrayTools:
     def flipHorizontally(arr):
         res = np.array(arr)[:, ::-1]
         return res.tolist()
-
+    def copy2Clipboard(arr):
+        import pyperclip
+        pyperclip.copy(str(arr))
+    def subGrid(arr: List[List[int]], point1: Tuple[int, int], point2: Tuple[int, int]):
+        x1,y1 = point1
+        x2,y2 = point2
+        return [row[y1:y2+1] for row in arr[x1:x2+1]]
+    def bounding_rect(obj: Set[Tuple[int, int]]):
+        min_r = min([x[0] for x in obj])
+        min_c = min([x[1] for x in obj])
+        max_r = max([x[0] for x in obj])
+        max_c = max([x[1] for x in obj])
+        return (min_r, min_c), (max_r, max_c)
+    def getArea(arr: List[List[int]]):
+        return len(arr) * len(arr[0])
+    def shape(arr: List[List[int]]):
+        return len(arr), len(arr[0])
+    def flatten(arr: List[List[int]]):
+        return [x for row in arr for x in row]
 class Field:
     def __init__(self, arr):
         self.arr = np.array(arr, dtype=int)
