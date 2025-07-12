0fb1fbfb5ef0dd36cfffe65df232e8f660f3e6e7
raja <rajababuchauhan500@gmail.com>
2025-07-12 21:12:54 +0200

    implemented objectedness

---
+++ src/objectedness.py
@@ -0,0 +1,82 @@
+from collections import deque
+from typing import List, Set, Tuple
+from .tools import ArrayTools
+class GridObject:
+    def set_grid(self, grid):
+        self.grid = grid
+    def set_objects(self, obj):
+        self.obj = obj
+        self.bounding_rect = ArrayTools.bounding_rect(obj)
+        self.rect_obj = ArrayTools.subGrid(self.grid, self.bounding_rect[0], self.bounding_rect[1])
+        self.shape = ArrayTools.shape(self.rect_obj)
+        x,y = self.shape
+        self.area =x*y
+        p = ArrayTools.flatten(self.rect_obj)
+        self.uid = ",".join(map(str, p))
+        k = set(p)
+        k.remove(0)
+        self.value = k.pop()
+    def replace_value(self, new_value):
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                if self.rect_obj[i][j] == self.value:
+                    self.rect_obj[i][j] = new_value
+        self.value = new_value
+class GridObjectGetter:
+    def __init__(self):
+        self.set_diagonal(False)
+    def set_diagonal(self, diagonal: bool ):
+        self.is_diagonal = diagonal
+        self._directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        if diagonal:
+            self._directions.extend([(-1, -1), (-1, 1), (1, -1), (1, 1)])
+    def set_grid(self, grid):
+        self.grid = grid
+        self.rows, self.cols = len(grid), len(grid[0])
+    def is_valid(self, r: int, c: int) -> bool:
+        grid = self.grid
+        return 0 <= r < self.rows and 0 <= c < self.cols and grid[r][c] != 0 and (r, c) not in self._visited
+    def flood_fill(self, start_r: int, start_c: int) -> Set[Tuple[int, int]]:
+        """Find all connected pixels using BFS."""
+        component = set()
+        queue = deque([(start_r, start_c)])
+        while queue:
+            r, c = queue.popleft()
+            if (r, c) in self._visited:
+                continue
+            component.add((r, c))
+            self._visited.add((r, c))
+            
+            for dr, dc in self._directions:
+                new_r, new_c = r + dr, c + dc
+                if self.is_valid(new_r, new_c) and self.is_same_condition((r, c), (start_r, start_c), component):
+                    queue.append((new_r, new_c))
+        
+        return component
+    def extract_objects(self) -> List[Set[Tuple[int, int]]]:
+        self._visited = set()
+        objects = []
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.grid[r][c] != 0 and (r, c) not in self._visited:
+                    component = self.flood_fill(r, c)
+                    objects.append(component)
+        
+        res = []
+        for obj in objects:
+            gb = GridObject()
+            gb.set_grid(self.grid)
+            gb.set_objects(obj)
+            res.append(gb)
+        return res
+    def get_val (self, point: Tuple[int, int]) -> int:
+        return self.grid[point[0]][point[1]]
+    def is_same_condition(self, point: Tuple[int, int], other_point: Tuple[int, int], *args) -> bool:
+        return self.get_val(point) == self.get_val(other_point)
+class Main:
+    @staticmethod
+    def get_objs(arr, diagonal=False):
+        go = GridObjectGetter()
+        go.set_grid(arr)
+        go.set_diagonal(diagonal)
+        return go.extract_objects()
