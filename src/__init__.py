25d18a7b12784fc6040ae4ebc9643b14f15a6f84
raja <rajababuchauhan500@gmail.com>
2025-07-15 19:14:13 +0200

    import adjustment

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .rlib.timeline.t2025.July.arc_agi_2.reader import Reader
+from .rlib.timeline.t2025.July.arc_agi_2.tools import Labels, ArrayTools, Field
+from .rlib.timeline.t2025.July.arc_agi_2.tools import ArcQuestion, Labels, ColorMap
+from .rlib.timeline.t2025.July.arc_agi_2.objectedness import Main as GridMain, GridObjectGetter, GridObject
+from .rlib.basic import Main as ObjMaker
+from .rlib.timeline.t2025.July.arc_agi_2.twoD_tools import Vector
